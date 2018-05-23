4dedf0cadcb2166ebfda63210a8a8bc840533900
Benjamin Ballesteros <jballesteros.mtz@gmail.com>
2018-05-22 20:57:53 -0500

    Initial Commit

---
+++ LoadingView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoadingView
+//
+//  Created by Joel Bejamin Ballesteros Martinez on 5/22/18.
+//  Copyright © 2018 Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
